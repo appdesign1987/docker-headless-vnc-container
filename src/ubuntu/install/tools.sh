74dc1fbc44a156571351a37c1f767836b5f2f377
appdesign1987 <jeroen@jeroenvd.nl>
2019-09-04 13:49:14 +0200

    Update tools.sh

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update 
-apt-get install -y vim wget net-tools locales bzip2 ssh filezilla \
+apt-get install -y vim wget net-tools locales bzip2 ssh filezilla dnsutils\
     python-numpy #used for websockify/novnc
 apt-get clean -y
 
